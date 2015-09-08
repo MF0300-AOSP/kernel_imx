3911a9dd7a3e46ab7e658c3dfea1aa561bb4e9c1
Shih-Yuan Huang <timmy@fic.com.tw>
2015-09-08 15:31:51 +0800

    MF0300:Add Cash Drawer Sensor Device and Modify switch-gpio driver for Device Tree Arch.

--- drivers/switch/switch_gpio.c
+++ drivers/switch/switch_gpio.c
@@ -23,6 +23,8 @@
 #include <linux/slab.h>
 #include <linux/switch.h>
 #include <linux/workqueue.h>
+#include <linux/of_platform.h>
+#include <linux/of_gpio.h>
 #include <linux/gpio.h>
 
 struct gpio_switch_data {
@@ -43,6 +45,7 @@ static void gpio_switch_work(struct work_struct *work)
 		container_of(work, struct gpio_switch_data, work);
 
 	state = gpio_get_value(data->gpio);
+	if (state) state=1;
 	switch_set_state(&data->sdev, state);
 }
 
@@ -76,20 +79,34 @@ static int gpio_switch_probe(struct platform_device *pdev)
 	struct gpio_switch_data *switch_data;
 	int ret = 0;
 
-	if (!pdata)
-		return -EBUSY;
-
 	switch_data = kzalloc(sizeof(struct gpio_switch_data), GFP_KERNEL);
 	if (!switch_data)
 		return -ENOMEM;
 
-	switch_data->sdev.name = pdata->name;
-	switch_data->gpio = pdata->gpio;
-	switch_data->name_on = pdata->name_on;
-	switch_data->name_off = pdata->name_off;
-	switch_data->state_on = pdata->state_on;
-	switch_data->state_off = pdata->state_off;
-	switch_data->sdev.print_state = switch_gpio_print_state;
+	if (pdata){
+
+		switch_data->sdev.name = pdata->name;
+		switch_data->gpio = pdata->gpio;
+		switch_data->name_on = pdata->name_on;
+		switch_data->name_off = pdata->name_off;
+		switch_data->state_on = pdata->state_on;
+		switch_data->state_off = pdata->state_off;
+		switch_data->sdev.print_state = switch_gpio_print_state;
+	}
+	else{
+		struct device_node *np = pdev->dev.of_node;
+		switch_data->sdev.name = of_get_property(np, "label", NULL) ? : pdev->name;
+		switch_data->gpio = of_get_named_gpio(np, "gpios", 0);
+		if (!gpio_is_valid(switch_data->gpio)) {
+			dev_err(pdev, "no sensor pwdn pin available\n");
+			return -ENODEV;
+		}
+		switch_data->name_on = of_get_property(np, "name_on", NULL);
+		switch_data->name_off = of_get_property(np, "name_off", NULL);
+		switch_data->state_on = of_get_property(np, "state_on", NULL);
+		switch_data->state_off = of_get_property(np, "state_off", NULL);
+		switch_data->sdev.print_state = switch_gpio_print_state;
+	}
 
 	ret = switch_dev_register(&switch_data->sdev);
 	if (ret < 0)
@@ -112,7 +129,7 @@ static int gpio_switch_probe(struct platform_device *pdev)
 	}
 
 	ret = request_irq(switch_data->irq, gpio_irq_handler,
-			  IRQF_TRIGGER_LOW, pdev->name, switch_data);
+			IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, pdev->name, switch_data);
 	if (ret < 0)
 		goto err_request_irq;
 
@@ -145,27 +162,22 @@ static int gpio_switch_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static const struct of_device_id of_gpio_swtich_match[] = {
+	{ .compatible = "gpio-switch", },
+	{},
+};
+
 static struct platform_driver gpio_switch_driver = {
 	.probe		= gpio_switch_probe,
 	.remove		= gpio_switch_remove,
 	.driver		= {
 		.name	= "switch-gpio",
 		.owner	= THIS_MODULE,
+		.of_match_table = of_match_ptr(of_gpio_swtich_match),
 	},
 };
 
-static int __init gpio_switch_init(void)
-{
-	return platform_driver_register(&gpio_switch_driver);
-}
-
-static void __exit gpio_switch_exit(void)
-{
-	platform_driver_unregister(&gpio_switch_driver);
-}
-
-module_init(gpio_switch_init);
-module_exit(gpio_switch_exit);
+module_platform_driver(gpio_switch_driver);
 
 MODULE_AUTHOR("Mike Lockwood <lockwood@android.com>");
 MODULE_DESCRIPTION("GPIO Switch driver");
